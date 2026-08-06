48b877ff9d94dd247986c2a2ee48d46893bf5e4b
snailcreature <samuel.drage@myport.ac.uk>
2026-08-06 17:31:27 +0100

    Abstracted script imports to improve performance

--- src/main.py
+++ src/main.py
@@ -1,17 +1,38 @@
 from argparse import ArgumentParser, Namespace
 
-from new import newOpt
-from init import initOpt
-from install import installOpt
-from uninstall import uninstallOpt
-from run import runOpt
-from build import buildOpt
-from love import loveOpt
-
 # amor version
 __version__ = '0.5.0'
 __author__ = 'Sam Drage (github: snailcreature)'
 __date__ = '2026-08-06'
+
+def _newOpt(args: Namespace):
+    from new import newOpt
+    newOpt(args)
+
+def _initOpt(args: Namespace):
+    from init import initOpt
+    initOpt(args)
+
+def _installOpt(args: Namespace):
+    from install import installOpt
+    installOpt(args)
+
+def _uninstallOpt(args: Namespace):
+    from uninstall import uninstallOpt
+    uninstallOpt(args)
+
+def _runOpt(args: Namespace):
+    from run import runOpt
+    runOpt(args)
+
+def _loveOpt(args: Namespace):
+    from love import loveOpt
+    loveOpt(args)
+
+def _buildOpt(args: Namespace):
+    from build import buildOpt
+    buildOpt(args)
+
 
 
 def defaultOpt(args: Namespace):
@@ -36,7 +57,7 @@ newParse.add_argument("name", nargs=1, type=str, help="The name of the\
         project. A directory with this name will be created.")
 newParse.add_argument("--git-init", "-g", action="store_true", help="Initialise\
         git repository in project.")
-newParse.set_defaults(func=newOpt)
+newParse.set_defaults(func=_newOpt)
 
 # Init
 init = subparsers.add_parser("init", help="Initialise an Amor project.")
@@ -46,7 +67,7 @@ init.add_argument("--force", '-f', action="store_true", help="Force the\
                   creation of a fresh amor.toml config file.")
 init.add_argument("--git-init", '-g', action="store_true", help="Initialise\
         git repository in project.")
-init.set_defaults(func=initOpt)
+init.set_defaults(func=_initOpt)
 
 # Install
 install = subparsers.add_parser("install", aliases=["i", "add"], help="Install\
@@ -59,7 +80,7 @@ install.add_argument("module", nargs="*", type=str, help="Module(s) to install\
                      the most current version will be installed. If <tag> is\
                      present, but does not exist on repository, the most\
                      current version will be installed.")
-install.set_defaults(func=installOpt)
+install.set_defaults(func=_installOpt)
 
 # Uninstall
 uninstall = subparsers.add_parser('uninstall', aliases=['u', 'remove'], help="\
@@ -67,25 +88,25 @@ uninstall = subparsers.add_parser('uninstall', aliases=['u', 'remove'], help="\
 uninstall.add_argument('module', nargs="+", type=str, help="Module(s) to\
         uninstall, given in format `<module_name>` (as you would require\
         in a Lua script).")
-uninstall.set_defaults(func=uninstallOpt)
+uninstall.set_defaults(func=_uninstallOpt)
 
 # Run
 run = subparsers.add_parser("run", aliases=["r"], help="Run a script defined in\
         the project amor.toml config file.")
 run.add_argument("script", nargs=1, type=str, help="Name of script to run.")
-run.set_defaults(func=runOpt)
+run.set_defaults(func=_runOpt)
 
 # Build
 build = subparsers.add_parser("build", aliases=["b"], help="Build project into\
         single directory for Löve.")
 build.add_argument("--clean", "-c", action="store_true", help="Remove existing\
                    build folder contents.")
-build.set_defaults(func=buildOpt)
+build.set_defaults(func=_buildOpt)
 
 # Love
 love = subparsers.add_parser("love", help="Run Löve using the current project's\
         build directory.")
-love.set_defaults(func=loveOpt)
+love.set_defaults(func=_loveOpt)
 
 args = parser.parse_args()
 args.func(args)
