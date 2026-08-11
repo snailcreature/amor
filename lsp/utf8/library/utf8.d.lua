69bd569c88d75dd7dc214ee1fc53702eef27d6e9
snailcreature <samuel.drage@myport.ac.uk>
2026-08-12 00:38:03 +0100

    Added utf8 (lua 5.3) definitions

---
+++ lsp/utf8/library/utf8.d.lua
@@ -0,0 +1,82 @@
+---@meta
+
+---This library provides basic support for UTF-8 encoding. It provides all its
+---functions inside the table utf8. This library does not provide any support for
+---Unicode other than the handling of the encoding. Any operation that needs the
+---meaning of a character, such as character classification, is outside its scope.
+---
+---Unless stated otherwise, all functions that expect a byte position as a parameter
+---assume that the given position is either the start of a byte sequence or one
+---plus the length of the subject string. As in the string library, negative indices
+---count from the end of the string.
+---
+---[Open in browser](https://www.lua.org/manual/5.3/manual.html#6.5)
+---
+---@class utf8
+utf8 = {}
+
+---Receives zero or more integers, converts each one to its corresponding UTF-8
+---byte sequence and returns a string with the concatenation of all these sequences.
+---@param ... integer[]
+---@return string
+function utf8.char(...) end
+
+---The pattern (a string, not a function) `"[\0-\x7F\xC2-\xF4][\x80-\xBF]*"`, which
+---matches exactly one UTF-8 byte sequence, assuming that the subject is a valid
+---UTF-8 string.
+---
+---@type string
+utf8.charpattern = "[\0-\x7F\xC2-\xF4][\x80-\xBF]*"
+
+---Returns values so that the construction
+---```lua
+---for p, c in utf8.codes(s) do body end
+---```
+---will iterate over all characters in string s, with p being the position (in bytes)
+---and c the code point of each character. It raises an error if it meets any invalid
+---byte sequence.
+---
+---@param s string
+---@return fun(s: string, p: integer):integer, integer
+function utf8.codes(s) end
+
+---Returns the codepoints (as integers) from all characters in s that start between
+---byte position i and j (both included). The default for i is 1 and for j is i.
+---It raises an error if it meets any invalid byte sequence.
+---
+---@param s string
+---@param i integer? Default `1`
+---@param j integer? Default `i`
+---@return integer[]
+function utf8.codepoint(s, i, j) end
+
+---Returns the number of UTF-8 characters in string s that start between positions
+---i and j (both inclusive). The default for i is 1 and for j is -1. If it finds
+---any invalid byte sequence, returns a false value plus the position of the first
+---invalid byte.
+---
+---@param s string
+---@param i integer? Default `1`
+---@param j integer? Default `-1`
+---@return [false, integer]|nil
+function utf8.len(s, i, j) end
+
+---Returns the position (in bytes) where the encoding of the n-th character of s
+---(counting from position i) starts. A negative n gets characters before position
+---i. The default for i is 1 when n is non-negative and #s + 1 otherwise, so that
+---utf8.offset(s, -n) gets the offset of the n-th character from the end of the
+---string. If the specified character is neither in the subject nor right after
+---its end, the function returns nil.
+---
+---As a special case, when n is 0 the function returns the start of the encoding
+---of the character that contains the i-th byte of s.
+---
+---This function assumes that s is a valid UTF-8 string.
+---
+---@param s string
+---@param n integer
+---@param i integer?
+---@return integer|nil
+function utf8.offset(s, n, i) end
+
+return utf8
